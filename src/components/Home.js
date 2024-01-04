22031814824af302ab4c97a9fae6f92924688184
mudit247365 <guptmudit2005@gmail.com>
2024-01-04 11:27:18 +0530

    ok

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <header id="home" class="header">
+        <div class="overlay"></div>
+        <div class="header-content container"></div>
+
+</header>
+      {/* <!-- end of page header --> */}
+    </div>
+  );
+}
+<div class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-primary">
+    Left
+  </button>
+  <button type="button" class="btn btn-primary">
+    Middle
+  </button>
+  <button type="button" class="btn btn-primary">
+    Right
+  </button>
+</div>;
---
+++ src/components/two.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function two() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
