0fa2e9e9802930d26f55a3668cbe70a5563f74a4
mudit247365 <guptmudit2005@gmail.com>
2024-01-09 09:42:32 +0530

    button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,20 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
     <div>
-      <header id="home" class="header">
+      <header id="home" class="header" >
         <div class="overlay"></div>
-        <div class="header-content container"></div>
+        <div class="header-content container" className="d-flex justify-content-center align-items-center vh-100"></div>
+        <div className="d-flex justify-content-center align-items-center vh-100 ">
+<button type="button" style={{width:'300px',height:'50px', backgroundColor:'black'}} className="btn btn-primary mx-3 ">Newspaper</button>
+<button type="button" style={{width:'300px',height:'50px', backgroundColor:'black'}} className="btn btn-success mx-3">Notes</button>
+<button type="button" style={{width:'300px',height:'50px', backgroundColor:'black'}} className="btn btn-danger mx-3">Premium Pdfs</button>
+</div>
 
 </header>
-      {/* <!-- end of page header --> */}
     </div>
   );
 }
-<div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-primary">
-    Left
-  </button>
-  <button type="button" class="btn btn-primary">
-    Middle
-  </button>
-  <button type="button" class="btn btn-primary">
-    Right
-  </button>
-</div>;
+
